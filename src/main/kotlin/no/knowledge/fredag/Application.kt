f94fa8ccedc3ea4e743dea270bdea05caf852a27
Stian Hundhammer <stian@knowledge.no>
2022-11-02 00:29:06 +0100

    running locally

--- src/main/kotlin/no/knowledge/fredag/Application.kt
+++ src/main/kotlin/no/knowledge/fredag/Application.kt
@@ -1,5 +1,23 @@
 package no.knowledge.fredag
 
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
 fun main() {
     println("hello!")
+    embeddedServer(Netty, port = 8080, host = "localhost") {
+        configureRouting()
+        println("configure now, baby!")
+    }.start(wait = true)
+}
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("hello, bastard!")
+        }
+    }
 }
